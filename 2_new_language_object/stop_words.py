8b1a4cabfb4098bf945e21154445353898918b63
Temitayo Olatoye <80205161+jolietemi@users.noreply.github.com>
2022-01-12 08:10:37 -0500

    Update stop_words.py

--- 2_new_language_object/stop_words.py
+++ 2_new_language_object/stop_words.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 
 STOP_WORDS = set(
-    """
+    """ 
+    . , tí ni wọn àwọn tó pé ń ṣe náà ó kò sí bá wá fi kí lọ yìí kan a jẹ́ sì fún ti bí yóò sọ àti rẹ láti wóṇ  ẹ ní í ló máa òun gbà nínú rí gbogbo nígbà lè ọmọ gbé ọ̀rọ̀ èyí mo mọ mọ́ rẹ̀ di ọ̀hún bẹ́ẹ̀ tún nǹkan ara nítorí ṣùgbọ́n lọ́wọ́ mú dá lórí ẹni ọwọ́ bíi jù pẹ̀lú ọjọ́ mi sílẹ̀ iṣẹ́ bó ohun kó un dé báyìí pa níbẹ̀ ibi wà ká tàbí láàrin yẹ gbọ́ làwọn bẹ̀rẹ̀ má ìyẹn kú ìgbà kọjá lóun eré ọnà pọ̀ jáde gẹ́gẹ́ jọ á i sọ̀rọ̀ fẹ́ irú kúrò bọ̀ o le lára níbi lẹ́yìn wò wo nípa ín inú méjì nílé yín ọn ńkọ́ ńlá já déédé la tẹ rárá ìdí òpin ṣeé lọ́jọ́ lọ́sẹ̀  títí wáyé ò padà án lójú tẹ́lẹ̀ rò gan tàwọn tán rán tòun lọ ẹ̀yin ọ̀dọ̀ wa tó kọ́kọ́ ta sá n yọ dúró hàn ṣiṣẹ́ lọ́dún kìí lẹ ṣẹlẹ̀ pàápàá nílùú nìkan níṣẹ́ síbẹ̀ nìyẹn yẹn kankan bóyá múra fáwọn e lọ́nà yá gbọ́dọ̀ lẹ́nu wí káàkiri parí síbi kọ ọ̀kan san péré dáadáa láìpẹ́ wọlé sùn tilẹ̀ lélẹ̀ ẹnikẹ́ni á ẹnìkan èmi ọ̀pọ̀ nídìí ún kínní ú sínú sáré fúnra díẹ̀ lòótọ́ àtàwọn bákan àbí kiri torí jẹ káwọn jùlọ ọ̀pọ̀lọpọ̀ síwájú lásán tọ tètè àwa pàdé tiẹ tuntun gba sọ́dọ̀ kàn yí ọ mìíràn wọ̀nyí ná ọ́ ẹ́ àfi 
 """.split()
 )
