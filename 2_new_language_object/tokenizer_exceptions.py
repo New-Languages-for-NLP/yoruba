c896756f879d3a43412c2fe3db46fa0fb21585bc
Andrew Janco <apjanco@gmail.com>
2022-05-05 13:53:43 -0400

    adding commas to address SyntaxError

--- 2_new_language_object/tokenizer_exceptions.py
+++ 2_new_language_object/tokenizer_exceptions.py
@@ -8,20 +8,20 @@ exclusions =[
 {'kínni':[{ORTH: 'kín'},{ORTH: 'ni'}]},
 {'èwoni':[{ORTH: 'èwo'},{ORTH: 'ni'}]},
 {"jẹ́kí":[{ORTH: "jẹ́"},{ORTH: 'kí'}]},
-{"wípé":[{ORTH: "wí"},{ORTH: 'pé'}]}
+{"wípé":[{ORTH: "wí"},{ORTH: 'pé'}]},
 {'gégẹ́ḅí':[{ORTH: 'gégẹ́'},{ORTH: 'ḅí'}]},
 {'ẹnití':[{ORTH: 'ẹni'},{ORTH: 'tí'}]},
 {'ibití':[{ORTH: 'ibi'},{ORTH: 'tí'}]},
 {'nítorínáà':[{ORTH: 'nítorí'},{ORTH: 'náà'}]},
 {"nítorítí":[{ORTH: "nítorí"},{ORTH: 'tí'}]},
-{"nígbàtí":[{ORTH: "nígbà"},{ORTH: 'tí'}]}
+{"nígbàtí":[{ORTH: "nígbà"},{ORTH: 'tí'}]},
 {'nígbànáà':[{ORTH: 'nígbà'},{ORTH: 'náà'}]},
 {"nígbàgbogbo":[{ORTH: "nígbà"},{ORTH: 'gbogbo'}]},
-{"léhìnnáà":[{ORTH: "léhìn"},{ORTH: 'náà'}]}
-{"ẹnipé":[{ORTH: "ẹni"},{ORTH: 'pé'}]}
+{"léhìnnáà":[{ORTH: "léhìn"},{ORTH: 'náà'}]},
+{"ẹnipé":[{ORTH: "ẹni"},{ORTH: 'pé'}]},
 {'nígbànáà':[{ORTH: 'nígbà'},{ORTH: 'náà'}]},
 {"nígbàgbogbo":[{ORTH: "nígbà"},{ORTH: 'gbogbo'}]},
-{"léhìnnáà":[{ORTH: "léhìn"},{ORTH: 'náà'}]}
+{"léhìnnáà":[{ORTH: "léhìn"},{ORTH: 'náà'}]},
 ]
 
 TOKENIZER_EXCEPTIONS = update_exc(BASE_EXCEPTIONS, *exclusions)
